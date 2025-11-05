aacb21157233830cc6addd2f10c16c6dc5c2d642
Hussain Murtaza <hmdesigneryeh@gmail.com>
2025-11-05 19:46:54 +0500

    new

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 alert("HEY EVERYONE");
 console.log("for ruff");
+
+var number = 12;
+
+if (number % 2 === 0) {
+    alert ("the number is even");
+} else {
+    alert ("the number in odd");
+}
+
+
